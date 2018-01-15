0caa391fa4d3768290a2f70cceceee35dad4711d
VSeryi <vseryi_vr13@hotmail.com>
2018-01-15 22:23:31 +0100

    Refactor NetworkUtils

--- launcher/project/src/main/java/com/svaleror/unionlulauncher/util/NetworkUtils.java
+++ launcher/project/src/main/java/com/svaleror/unionlulauncher/util/NetworkUtils.java
@@ -6,6 +6,7 @@
 package com.svaleror.unionlulauncher.util;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
@@ -20,89 +21,85 @@ import javax.net.SocketFactory;
  */
 public class NetworkUtils {
 
-    public static boolean isInternetOn() {
-        boolean result = false;
-        try {
-            URL url = new URL("http://www.google.com");
-            HttpURLConnection con = (HttpURLConnection) url
-                    .openConnection();
-            con.connect();
-            if (con.getResponseCode() == 200) {
-                result = true;
-            }
-        } catch (Exception e) {
-            result = false;
-        }
-        return result;
-    }
+	private static String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
 
-    public static boolean isServerOn() {
-        boolean open = true;
-        try {
-            Socket socket = SocketFactory.getDefault().createSocket();
-            socket.setSoTimeout(5000);
-            socket.connect(new InetSocketAddress("unionlu.duckdns.org", 25565));
-            socket.close();
-        } catch (Exception e) {
-            open = false;
-        }
-        return open;
-    }
+	private static String sendGet(String url) throws IOException {
 
-    public static int makeLogin(String user, String password) {
-        int result = 0;
-        try {
-            String loginUrl = "http://unionlu.duckdns.org/login.php?user=#{username}&password=#{password}";
-            String loginOk = "loginWasOK";
-            String loginBad = "loginWasBAD";
-            loginUrl = loginUrl.replace("#{username}", user);
-            loginUrl = loginUrl.replace("#{password}", password);
-            URL url = new URL(loginUrl);
-            HttpURLConnection con = (HttpURLConnection) url
-                    .openConnection();
-            con.connect();
-            if (con.getResponseCode() == 200) {
-                BufferedReader r = new BufferedReader(new InputStreamReader(con.getInputStream(), Charset.forName("UTF-8")));
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = r.readLine()) != null) {
-                    sb.append(line);
-                }
-                if (!sb.toString().contains(loginBad) && sb.toString().contains(loginOk)) {
-                     result = 1;
-                }
-            } else {
-                result = -1;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getStackTrace()[0]);
-            result = -1;
-        }
-        return result;
-    }
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-    public static String getLastVersion() {
-        String result = "-1";
-        try {
-            String versionUrl = "https://raw.githubusercontent.com/VSeryi/mcunionlu/master/version.txt";
-            URL url = new URL(versionUrl);
-            HttpURLConnection con = (HttpURLConnection) url
-                    .openConnection();
-            con.connect();
-            if (con.getResponseCode() == 200) {
-                BufferedReader r = new BufferedReader(new InputStreamReader(con.getInputStream(), Charset.forName("UTF-8")));
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = r.readLine()) != null) {
-                    sb.append(line);
-                }
-                result = sb.toString();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            result = "-1";
-        }
-        return result;
-    }
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		return response.toString();
+
+	}
+
+	public static boolean isInternetOn() {
+		boolean result = false;
+		try {
+			sendGet("http://www.google.com");
+			result = true;
+		} catch (Exception e) {
+			result = false;
+		}
+		return result;
+	}
+
+	public static boolean isServerOn() {
+		boolean open = true;
+		try {
+			Socket socket = SocketFactory.getDefault().createSocket();
+			socket.setSoTimeout(5000);
+			socket.connect(new InetSocketAddress("unionlu.duckdns.org", 25565));
+			socket.close();
+		} catch (Exception e) {
+			open = false;
+		}
+		return open;
+	}
+
+	public static int makeLogin(String user, String password) {
+		int result = 0;
+		try {
+			String loginUrl = "http://unionlu.duckdns.org/login_launcher.php?user=#{username}&password=#{password}";
+			String loginOk = "loginWasOK";
+			String loginBad = "loginWasBAD";
+			loginUrl = loginUrl.replace("#{username}", user);
+			loginUrl = loginUrl.replace("#{password}", password);
+			String page = sendGet(loginUrl);
+			if (!page.contains(loginBad) && page.contains(loginOk)) {
+				result = 1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getStackTrace()[0]);
+			result = -1;
+		}
+		return result;
+	}
+
+	public static String getLastVersion() {
+		String result = "-1";
+		try {
+			String versionUrl = "https://raw.githubusercontent.com/VSeryi/mcunionlu/master/version.txt";
+			result = sendGet(versionUrl);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = "-1";
+		}
+		return result;
+	}
 }
