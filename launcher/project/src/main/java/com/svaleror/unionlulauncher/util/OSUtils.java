aab9ee7f8c308ac1133fb22433ae52ed23061796
VSeryi <vseryi_vr13@hotmail.com>
2024-06-23 00:13:47 +0200

    New Launcher

--- launcher/project/src/main/java/com/svaleror/unionlulauncher/util/OSUtils.java
+++
@@ -1,8 +0,0 @@
-package com.svaleror.unionlulauncher.util;
-
-public class OSUtils {
-	public static boolean isWindows() {
-		return System.getProperty("os.name").startsWith("Windows");
-	}
-
-}
--- modpackupdater/project/src/main/java/com/svaleror/modpackupdater/util/OSUtils.java
+++
@@ -1,8 +0,0 @@
-package com.svaleror.modpackupdater.util;
-
-public class OSUtils {
-	public static boolean isWindows() {
-		return System.getProperty("os.name").startsWith("Windows");
-	}
-
-}
